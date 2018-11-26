ea44b13c1eab3cf33c5015f5528ff3acacc2b9ee
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-26 19:05:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,9 @@ def key_for_min_value(name_hash)
     return nil
   else
     min_amt = name_hash.first[1]
-    # binding.pry
     name_hash.each do |key,value|
 
-    if v < smallest_value
-      smallest_value = v
+    min_amt = value if value < min_amt
     end
 
     end
