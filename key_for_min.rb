373bf2a0fc1647ba7bc232635c9fd5990ae2e59d
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-26 19:11:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   else
+    min_key = name_hash.first[0]
     min_amt = name_hash.first[1]
     
     name_hash.each do |key,value|
